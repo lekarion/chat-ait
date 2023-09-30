301c27d04f567125f61986faadd3b5f08015ee2e
lekarion <lekarion.dev@gmail.com>
2023-09-30 23:45:34 +0300

    chore: area of responsibility corrected for different level coordinators

--- Project/ChatAIT/App/AppCoordinator.swift
+++ Project/ChatAIT/App/AppCoordinator.swift
@@ -5,23 +5,47 @@
 //  Created by developer on 03.09.2023.
 //
 
+import CocoaLumberjack
+import DecouplingLabSDK
 import UIKit
 
 protocol AppCoordinatorInterface: AnyObject {
     func propagateViewController(main viewController: UIViewController)
 }
 
-class AppCoordinator: AppCoordinatorInterface {
+class AppCoordinator {
+    init() {
+        guard let identifier = Bundle.main.bundleIdentifier else {
+            fatalError("Internal inconsistency")
+        }
+
+        DecouplingLab.start(with: identifier)
+        DecouplingLab.registerAssistants { context in
+            AssistantsFactory.allDescriptors.forEach { descriptor in
+                descriptor.registrationHandler(context)
+            }
+        }
+
+        DecouplingLab.allAssistantIdentifiers.forEach { identifier in
+            DecouplingLab.enableAssistant(for: identifier, true)
+            DDLogVerbose("\(Self.logPrefix) assistant with identifier '\(identifier)' is enabled")
+        }
+    }
+
+
+    deinit {
+        charCoordinator?.stop()
+        DecouplingLab.stop()
+    }
+
+    private var charCoordinator: ChatCoordinator?
+}
+
+extension AppCoordinator: AppCoordinatorInterface {
     func propagateViewController(main viewController: UIViewController) {
         guard nil == charCoordinator else { return }
 
         charCoordinator = ChatCoordinator(viewController: viewController)
         charCoordinator?.start()
     }
-
-    deinit {
-        charCoordinator?.stop()
-    }
-
-    private var charCoordinator: ChatCoordinator?
 }
