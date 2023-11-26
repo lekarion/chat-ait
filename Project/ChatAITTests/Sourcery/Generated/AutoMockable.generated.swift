1a8fdbed2b0243640d7b1ed0924470e80eb85baa
lekarion <lekarion.dev@gmail.com>
2023-11-27 00:29:57 +0200

    feat: generate unit tests target

---
+++ Project/ChatAITTests/Sourcery/Generated/AutoMockable.generated.swift
@@ -0,0 +1,10 @@
+// Generated using Sourcery 2.1.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+import Foundation
+import Combine
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import AppKit
+#endif
