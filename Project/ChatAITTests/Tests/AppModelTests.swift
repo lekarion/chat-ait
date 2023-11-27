582280b810f894b951f6e1476e39c59892630f6e
lekarion <lekarion.dev@gmail.com>
2023-11-28 00:44:21 +0200

    feat: a few simple model tests implemented

--- Project/ChatAITTests/Tests/AppModelTests.swift
+++ Project/ChatAITTests/Tests/AppModelTests.swift
@@ -6,29 +6,83 @@
 //
 
 import XCTest
+import Combine
+
 @testable import ChatAIT
 
 class AppModelTests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        eventsQueue = DispatchQueue(label: "com.chatait.tests.eventQueue", qos: .utility)
+        model = ChatModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        bag.removeAll()
+        model = nil
+        eventsQueue = nil
+        currentExpectation = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testStartStop() throws {
+        XCTAssertEqual(model.state, .off, "Model should not be started yet")
+
+        model.modelUpdateEvent.receive(on: eventsQueue).sink { [weak self] reason in
+            guard let self = self else { return }
+
+            switch reason {
+            case .stateChanged(let state):
+                XCTAssertEqual(self.model.state, state)
+                print("\(#function): state = \(state)")
+                self.currentExpectation?.fulfill()
+            default:
+                break
+            }
+        }.store(in: &bag)
+
+        currentExpectation = XCTestExpectation(description: "Waiting for update event")
+        model.start()
+        XCTAssertEqual(model.state, .idle, "Model should be in idle state")
+        wait(for: [currentExpectation!], timeout: 5.0)
+        currentExpectation = nil
+
+        currentExpectation = XCTestExpectation(description: "Waiting for update event")
+        model.stop()
+        XCTAssertEqual(model.state, .off, "Model should be in off state")
+        wait(for: [currentExpectation!], timeout: 5.0)
+        currentExpectation = nil
+    }
+
+    func test8BallAssistant() throws {
+        XCTAssertGreaterThanOrEqual(AssistantsFactory.assistantIdentifiersMap.count, 1)
+
+        let assistantId = "com.assistants.magic8BallConversation"
+        XCTAssertNotNil(AssistantsFactory.assistantIdentifiersMap[assistantId])
+
+        XCTAssertEqual(model.state, .off)
+
+        model.start()
+        XCTAssertEqual(model.state, .idle)
+
+        model.startConversation(withAssistant: assistantId)
+        XCTAssertEqual(model.state, .assisting)
+
+        model.stopConversation()
+        XCTAssertEqual(model.state, .idle)
+
+        model.stop()
+        XCTAssertEqual(model.state, .off)
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
         }
     }
+
+    var model: ChatModel!
+    var bag = Set<AnyCancellable>()
+    var eventsQueue: DispatchQueue!
+    var currentExpectation: XCTestExpectation?
+
+    static let assistanIDs = ["com.assistants.magic8BallConversation", "com.assistants.dragonConversation"]
 }
